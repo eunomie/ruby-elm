bb49ced68103dea4356da7d757a77efbabaa829b
Yves Brissaud <yves.brissaud@gmail.com>
2016-03-01 14:18:34 +0100

    Start cli parser

---
+++ lib/elm/opt_parser.rb
@@ -0,0 +1,64 @@
+require 'contracts'
+require 'optparse'
+require 'options'
+
+module Elm
+  # Parse command line options
+  class OptParser
+    include Contracts::Core
+    include Contracts::Builtin
+
+    # rubocop:disable Metrics/AbcSize
+    # rubocop:disable Metrics/MethodLength
+    Contract [String] => Options
+    def self.parse(args)
+      options = Options.new
+
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = 'Usage: ruby-elm [FILES...] ' \
+          '[--output FILE] [--yes] [--report FORMAT] [--warn] [--docs FILE]'
+
+        opts.separator ''
+        opts.separator 'Available options:'
+
+        opts.on_tail('-h', '--help',
+                     'Show this help text') do
+          puts opts
+          exit
+        end
+
+        opts.on('--output [FILE]',
+                'Write result to the given .html or .js FILE.') do |output|
+          options.output = output
+        end
+
+        opts.on('--yes',
+                'Reply \'yes\' to all automated prompts.') do
+          options.yes = true
+        end
+
+        opts.on('--report [FORMAT]', [:normal, :json],
+                'Format of error and warning reports',
+                '  (normal or json)') do |format|
+          options.report = format
+        end
+
+        opts.on('--warn',
+                'Report warnings to improve code quality.') do
+          options.warn = true
+        end
+
+        opts.on('--docs [FILE]',
+                'Write documentation to FILE as JSON.') do |doc|
+          options.docs = doc
+        end
+
+        opts.separator ''
+        opts.separator 'Examples:'
+      end
+
+      opt_parser.parse! args
+      options
+    end
+  end
+end
---
+++ lib/elm/options.rb
@@ -0,0 +1,50 @@
+require 'contracts'
+
+module Elm
+  # Options to run elm
+  class Options
+    include Contracts::Core
+    include Contracts::Builtin
+
+    REPORTS = [:normal, :json].freeze
+
+    attr_reader :output, :yes, :report, :warn, :docs
+
+    def initialize
+      @output = 'index.html'
+      @yes = false
+      @report = :normal
+      @warn = false
+      @docs = nil
+    end
+
+    # rubocop:disable Style/TrivialAccessors
+    Contract String => String
+    def output=(value)
+      # check output is valid
+      @output = value
+    end
+
+    Contract Bool => Bool
+    def yes=(value)
+      @yes = value
+    end
+
+    Contract Symbol => Symbol
+    def report=(value)
+      raise Error if REPORTS.index?(value).nil?
+      @report = value
+    end
+
+    Contract Bool => Bool
+    def warn=(value)
+      @warn = value
+    end
+
+    Contract String => String
+    def docs=(value)
+      @docs = value
+    end
+    # rubocop:enable Style/TrivialAccessors
+  end
+end
