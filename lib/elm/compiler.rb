cfd3603caedd5463c1c94aea53bc89fd6a9318b1
Yves Brissaud <yves.brissaud@gmail.com>
2016-03-12 14:40:35 +0100

    Allow to compile elm from string

--- lib/elm/compiler.rb
+++ lib/elm/compiler.rb
@@ -1,4 +1,5 @@
 require 'contracts'
+require 'tempfile'
 require 'elm/runnable'
 require 'elm/files'
 
@@ -26,6 +27,25 @@ module Elm
                                                           docs:   Maybe[String] }] => Files # rubocop:disable Metrics/LineLength
     def files(files_list, with_options: {})
       files files_list, with_options: Elm::Options.with(with_options)
+    end
+    # rubocop:enable Lint/DuplicateMethods
+
+    Contract String, KeywordArgs[with_options: Optional[Elm::Options]] => Files # rubocop:disable Metrics/LineLength
+    def content(data, with_options: Elm::Options.new)
+      Tempfile.open(['elm', '.elm']) do |tempfile|
+        tempfile.write data
+        files [tempfile.path], with_options: with_options
+      end
+    end
+
+    # rubocop:disable Lint/DuplicateMethods
+    Contract String, KeywordArgs[with_options: { output: Maybe[String],
+                                                 yes:    Maybe[Bool],
+                                                 report: Maybe[Symbol],
+                                                 warn:   Maybe[Bool],
+                                                 docs:   Maybe[String] }] => Files # rubocop:disable Metrics/LineLength
+    def content(data, with_options: {})
+      content data, with_options: Elm::Options.with(with_options)
     end
     # rubocop:enable Lint/DuplicateMethods
 
